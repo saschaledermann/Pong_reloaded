9e86afafa7ac5edfab87651d4934fc8e4c34e1b2
Sascha Ledermann <sascha.ledermann@bfh.ch>
2024-01-30 13:38:09 +0100

    Added Control for mobile aspect ratios

---
+++ Assets/_Project/Scripts/OrthographicCamera.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrthographicCamera : MonoBehaviour
+{
+    Camera m_camera;
+
+    void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+
+        if (!m_camera.CompareTag("MainCamera"))
+            Debug.LogWarning("Camera is not set as Main Camera!");
+    }
+
+    void Start()
+    {
+        var (center, size) = CalculateOrthosize();
+
+        m_camera.transform.position = center;
+        m_camera.orthographicSize = size;
+    }
+
+    (Vector3 center, float size) CalculateOrthosize()
+    {
+        var bounds = new Bounds();
+
+        foreach (var col in FindObjectsOfType<Collider2D>())
+        {
+            bounds.Encapsulate(col.bounds);
+        }
+
+        var vertical = bounds.size.y;
+        var horizontal = bounds.size.x * m_camera.pixelHeight / m_camera.pixelWidth;
+
+        var size = Mathf.Max(horizontal, vertical) * 0.5f;
+        var center = bounds.center + new Vector3(0, 0, -10);
+
+        return (center, size);
+    }
+}
