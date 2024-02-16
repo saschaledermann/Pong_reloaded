efb623d940241e214eda6a961a277b2fdb305396
Sascha Ledermann <sascha.ledermann@bfh.ch>
2024-02-16 15:57:22 +0100

    Improved AI Input

--- Assets/_Project/Scripts/ComputerInput.cs
+++ Assets/_Project/Scripts/ComputerInput.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class ComputerInput : MonoBehaviour, IPaddleInput
 {
+    [Range(0f, 1f)] public float foresight = 0.5f;
     Ball m_ball;
     Vector2 m_input = new();
+    bool m_move;
 
     void Start()
     {
@@ -13,8 +15,17 @@ public class ComputerInput : MonoBehaviour, IPaddleInput
     public Vector2 GetInput()
     {
         if (m_ball == null) return Vector2.zero;
-        if (Mathf.Abs(m_ball.transform.position.x - transform.position.x) < 0.5f) return Vector2.zero;
-        m_input.x = m_ball.transform.position.x > transform.position.x ? 1f : -1f;
+        var yDistance = Mathf.Abs(m_ball.transform.position.y - transform.position.y);
+        var xDistance = Mathf.Abs(m_ball.transform.position.x - transform.position.x);
+
+        if (xDistance > 0.5f && yDistance < (7 * foresight)) m_move = true;
+        if (xDistance < 0.1f || yDistance > (7 * foresight)) m_move = false;
+        Debug.Log($"{yDistance}, {m_move}");
+
+        if (m_move)
+            m_input.x = m_ball.transform.position.x > transform.position.x ? 1f : -1f;
+        else
+            m_input = Vector2.zero;
         return m_input;
     }
 }
